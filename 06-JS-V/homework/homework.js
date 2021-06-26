59aa3b5a180b217e0a18953de0de7507cf6eab01
AleEspinozama <espinozalezama@gmail.com>
2021-06-25 19:45:00 -0500

    tarea 06 update

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -62,8 +62,9 @@ function agregarStringInvertida() {
       this.apellido= apellido,
       this.edad= edad,
       this.domicilio= domicilio  
-
-      this.detalle= function(){
+    }
+      
+    detalle(){
       return{
         Nombre: this.nombre, 
         Apellido: this.apellido,
@@ -72,7 +73,7 @@ function agregarStringInvertida() {
         } 
       }
     }
-  }
+ 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
